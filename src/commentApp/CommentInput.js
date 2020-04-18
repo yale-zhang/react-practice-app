a872bc9c87f0b7e034a22ed3e3b523fb1dd66da0
zys6024@163.com <zys6024@163.com>
2020-04-18 21:13:49 +0800

    [add]显示评论发布时间

--- src/commentApp/CommentInput.js
+++ src/commentApp/CommentInput.js
@@ -43,8 +43,11 @@ class CommentInput extends Component {
   }
   handleSubmit(){
     if(this.props.onSubmit){
-        const { username,content} =this.state
-        this.props.onSubmit({username,content})
+        this.props.onSubmit({
+          username: this.state.username,
+          content: this.state.content,
+          createdTime: +new Date()
+        })
     }
     this.setState({content:''})
   }
